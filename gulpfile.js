7e6292b2f489dc9e508eff9da0cfefe9eeb4d334
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-03-05 20:10:14 +0100

    Upgrading project

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const { src, dest, watch } = require("gulp");
+const sass = require("gulp-sass")(require('sass'));
+
+function css(done){
+    src("src/scss/app.scss")  // Identificar el archivo SASS
+    .pipe(sass())  // Compilarlo
+    .pipe(dest("build/css")); // Almacernala en el disco duro
+    done(); // Callback que avisa a gulp cuando llegamos al final
+}
+
+function dev(done){
+    watch("src/scss/app.scss", css);
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
