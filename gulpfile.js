5f55196d88bf5925b4b105304717812998d722cd
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-03-07 20:21:53 +0100

    Upgrading project

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
+const plumber = require ('gulp-plumber');
 
 function css(done){
-    src("src/scss/app.scss")  // Identificar el archivo SASS
+    src("src/scss/**/*.scss")  // Identificar el archivo SASS
+    .pipe(plumber())
     .pipe(sass())  // Compilarlo
     .pipe(dest("build/css")); // Almacernala en el disco duro
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
 function dev(done){
-    watch("src/scss/app.scss", css);
+    watch("src/scss/**/*.scss", css);
     done();
 }
 
