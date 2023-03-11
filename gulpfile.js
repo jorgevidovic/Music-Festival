a1548da735def2aaa1f882c2deff886240aa5d55
Jorge I. Vidovic <jorgeisaacvidovich@gmail.com>
2023-03-11 19:09:28 +0100

    Upgrading project

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+const imagemin = require("gulp-imagemin");
+
+//CSS
 const sass = require("gulp-sass")(require('sass'));
 const plumber = require ('gulp-plumber');
+
+// Images
+const cache = require ('gulp-cache');
+const imageMin = require ('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done){
     src("src/scss/**/*.scss")  // Identificar el archivo SASS
@@ -10,10 +19,45 @@ function css(done){
     done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
+function toImageMin(done){
+    const options = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imageMin(options)))
+    .pipe( dest('build/img'))
+    done();
+}
+
+function toWebp(done){
+    const options = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(webp(options))
+    .pipe(dest('build/img'));
+    done();
+}
+
+function toAvif(done){
+    const options = {
+        quality: 50
+    }
+
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(options))
+    .pipe(dest('build/img'));
+    done();
+}
+
 function dev(done){
     watch("src/scss/**/*.scss", css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.toImageMin = toImageMin;
+exports.toWebp = toWebp;
+exports.toAvif = toAvif;
+exports.dev = parallel(toImageMin, toWebp, toAvif, dev);
